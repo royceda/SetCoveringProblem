4a723c3998278bc6fc03100d467de425af220bb5
royceda <caucricity@gmail.com>
2017-01-02 10:19:09 -0800

    choice function

---
+++ couv/primal_heuristic.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+# define I[i]
+# define J[i]
+
+
+#define f
+norm  = 1 #card(I[j] union Ib) for all j in Jb
+
+f  = lambda x: c[j]/norm
+f2 = lambda x: c[j]/norm**2
+f3 = lambda x: np.sqrt(c[j])/norm
